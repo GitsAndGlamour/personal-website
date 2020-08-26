779cd86423d0dfa3a4f1af0e15818756b5b38d7b
Omie Walls <omierwalls@gmail.com>
2020-08-25 20:25:35 -0400

    Switched to Apollo GraphQL Client

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
+import gql from 'graphql-tag';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
 import awsExports from './aws-exports';
+import { ApolloProvider } from 'react-apollo';
+
+import { listPosts } from './graphql/queries';
+
+const client = new AWSAppSyncClient({
+    url: awsExports.aws_appsync_graphqlEndpoint,
+    cacheOptions: {},
+    region: awsExports.aws_appsync_region,
+    auth: {
+        type: AUTH_TYPE.API_KEY,
+        apiKey: awsExports.aws_appsync_apiKey,
+    }
+});
+
+client.query({
+    query: gql(listPosts)
+}).then(({ data }) => {
+    console.log(data);
+});
 
 Amplify.configure(awsExports);
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
