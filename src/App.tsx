7d05622e2f695005dae6f39aab8661e9c3e4f44f
Omie Walls <omierwalls@gmail.com>
2020-09-01 19:31:32 -0400

    Basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,46 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
+    Route, Link,
 } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
-      <Switch>
-          <Route path='/'><>Hello World</></Route>
-      </Switch>
+        <div>
+          <nav>
+              <ul>
+                  <li>
+                      <Link to='/'>Home</Link>
+                  </li>
+                  <li>
+                      <Link to='/blog'>Blog</Link>
+                  </li>
+                  <li>
+                      <Link to='/tutorials'>Tutorials</Link>
+                  </li>
+                  <li>
+                      <Link to='/portfolio'>Portfolio</Link>
+                  </li>
+                  <li>
+                      <Link to='/contact'>Contact Me</Link>
+                  </li>
+                  <li>
+                      <Link to='/hire'>Hire Me</Link>
+                  </li>
+              </ul>
+          </nav>
+          <Switch>
+              <Route path='/blog'><>Blog</></Route>
+              <Route path='/tutorials'><>Tutorials</></Route>
+              <Route path='/portfolio'><>Portfolio</></Route>
+              <Route path='/contact'><>Contact</></Route>
+              <Route path='/hire'><>Hire</></Route>
+              <Route path='/admin'><>Admin</></Route>
+              <Route path='/'><>Hello World</></Route>
+              <Route path='*'><>404 Page Not Found</></Route>
+          </Switch>
+        </div>
     </Router>
   );
 }
