258e5af7ef7923b8f9a2eef50f2f5b0b5a6291ae
Omie Walls <omierwalls@gmail.com>
2020-09-05 14:50:50 -0400

    Crud for admin blog finished

--- src/admin/Blog/AdminBlogList.tsx
+++ src/admin/Blog/AdminBlogList.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
-import {API} from "aws-amplify";
+import {API, graphqlOperation} from "aws-amplify";
 import {listBlogs} from "../../graphql/queries";
+import {deleteBlog} from "../../graphql/mutations";
 import {Blog} from "../../model/blog";
 import {Link} from "react-router-dom";
 
@@ -23,6 +24,16 @@ async function fetchBlogs() {
     console.log(blogs);
 }
 
+async function onDeleteBlog(id: string) {
+    // eslint-disable-next-line no-restricted-globals
+    const canDelete = confirm('Are you sure you want to delete this blog?');
+    if (canDelete) {
+        await API.graphql(graphqlOperation(deleteBlog, {input: {id}}));
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+    }
+}
+
     return (
         <div>
             <header>Blogs</header>
@@ -32,7 +43,10 @@ async function fetchBlogs() {
                         <h4><Link to={`/admin/blog/${blog.id}`}>{blog.title}</Link></h4>
                         <h5>{blog.subtitle}</h5>
                         <p>Posted on <b>{new Date(blog.posted).toDateString()}</b></p>
-                        <p><b>Tags: </b>{blog.tags.toString().replaceAll(',', ', ')} <Link to={`/admin/blog/form/${blog.id}`}><button>edit</button></Link></p>
+                        <p><b>Tags: </b>{blog.tags.toString().replaceAll(',', ', ')}
+                            <Link to={`/admin/blog/form/${blog.id}`}><button>edit</button></Link>
+                            <button onClick={(event: React.MouseEvent<HTMLButtonElement>) => onDeleteBlog(blog.id)}>delete</button>
+                        </p>
                     </li>
                 })}
             </ul>
